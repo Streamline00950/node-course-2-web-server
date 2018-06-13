085e4a8bb1ada57e9f0fb6e72b60f8e2250602fe
Streamline <lukaszmal00950@gmail.com>
2018-06-13 23:52:14 +0200

    Added projects page

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ app.get('/about',(req,res)=>{
     });
 });
 
+app.get('/projects', (req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle: 'Portfolio page here',
+        welcomeMsg: "Welcome to my website!"
+    });
+})
+
 app.get('/bad', (req,res)=>{
     res.send({
         errorMessage: 'Something went wrong :/' 
